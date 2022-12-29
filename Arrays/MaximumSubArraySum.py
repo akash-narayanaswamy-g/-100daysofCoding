dd85475773dcd9b02513f7336bdf86b4bddeab15
akash_narayanaswamy <akashnarayanaswamy.ng@gmail.com>
2022-12-29 21:58:48 +0530

    Day 16

---
+++ Arrays/MaximumSubArraySum.py
@@ -0,0 +1,14 @@
+#53. Maximum Subarray
+#https://leetcode.com/problems/maximum-subarray/description/
+
+class Solution(object):
+    def maxSubArray(self, A):
+        if not A:
+            return 0
+
+        curSum = maxSum = A[0]
+        for num in A[1:]:
+            curSum = max(num, curSum + num)
+            maxSum = max(maxSum, curSum)
+
+        return maxSum
