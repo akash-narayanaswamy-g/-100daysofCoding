d689e7fa031b171b924ca64ed01bae2f82abdbf7
akash_narayanaswamy <akashnarayanaswamy.ng@gmail.com>
2023-02-12 22:59:22 +0530

    Day 61

--- Arrays/DeleteGreatestValueInEachRow.java
+++ Arrays/DeleteGreatestValueInEachRow.java
@@ -1,10 +1,33 @@
 package ArrayProblems;
 
+import java.util.Arrays;
+
+//2500. Delete Greatest Value in Each Row
+//https://leetcode.com/problems/delete-greatest-value-in-each-row/
+
 public class DeleteGreatestValueInEachRow {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] grid = {{1,2,4},{3,3,1}};
+		System.out.println(deleteGreatestValue(grid));
 	}
+	
+	public static int deleteGreatestValue(int[][] grid) {
+        
+        for (int i = 0; i < grid.length; i++) {
+			Arrays.sort(grid[i]);
+		}
+        
+        int ans = 0;
+        for(int i=grid[0].length-1; i>=0; i--){
+            int temp = 0;
+            for(int j=0; j<grid.length; j++){
+                temp = Math.max(temp , grid[j][i]);
+            }
+            ans += temp;
+			
+		}
+        return ans;
+    }
 
 }
